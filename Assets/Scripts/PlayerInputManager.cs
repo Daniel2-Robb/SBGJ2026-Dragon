2d7ff1cc051ea9e89e41c3ba14e4cbb4a02dfba1
Daniel Robb <Daniel2.Robb@live.uwe.ac.uk>
2026-09-22 16:04:49 +0100

    Filled out remaining action setups

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -7,6 +7,8 @@ public class PlayerInputManager : MonoBehaviour
     GameManager manager;
     //[SerializeField] private InputActionAsset playerInput;
     public InputActionReference kill;
+    public InputActionReference attack1;
+    public InputActionReference attack2;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,11 +27,15 @@ public class PlayerInputManager : MonoBehaviour
     private void OnEnable()
     {
         kill.action.started += Kill;
+        attack1.action.started += Attack(1);
+        attack2.action.started += Attack(2);
     }
 
     private void OnDisable()
     {
         kill.action.started -= Kill;
+        attack1.action.started -= Attack(1);
+        attack2.action.started -= Attack(2);
     }
 
     private void Kill(InputAction.CallbackContext context)
@@ -37,4 +43,13 @@ public class PlayerInputManager : MonoBehaviour
         //Debug.Log("Action completed");
         manager.VillagerKilled(5);
     }
+
+    private Action<InputAction.CallbackContext> Attack(int attackIndex)
+    {
+        manager.PlayerAttackCall(attackIndex);
+
+        //throw new NotImplementedException();
+        return null;
+    }
+
 }
